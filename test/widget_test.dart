0974961cc76a1c457bcfacdd6395d7bca52593c9
Ohnao <naoya.ohsaki@gaiax.com>
2019-12-25 16:43:26 +0900

    アイコンの有無をテストするコードの追加

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter/material.dart';
 import 'package:test2_flutter/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     await tester.pumpWidget(new MyApp());
     await tester.pump();
+
+    expect(find.byIcon(Icons.add), findsOneWidget);
   });
 }
