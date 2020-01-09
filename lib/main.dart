ecf7bfed3ac662b3ea8dd18457737ddd19ff27bf
Ohnao <naoya.ohsaki@gaiax.com>
2020-01-09 11:47:42 +0900

    about RenderObjectWidget and RenderBox

--- lib/main.dart
+++ lib/main.dart
@@ -20,167 +20,50 @@ class MyHomePage extends StatefulWidget {
   @override
   _MyHomePageState createState() => new _MyHomePageState();
 }
+
 class _MyHomePageState extends State<MyHomePage> {
-  String _message = 'ok';
-  bool _switch = true;
-  double _slider = 0.0;
-
-  void buttonPressed(){
-    showDialog(
-      context: context,
-      builder: (BuildContext context) => CupertinoAlertDialog(
-        title: Text("Hello"),
-        content: const Text("This is sample"),
-        actions: <Widget>[
-          CupertinoDialogAction(
-            child: const Text('Cancel'),
-            onPressed: () => Navigator.pop<String>(context, 'cancel')
-          ),
-          CupertinoDialogAction(
-            child: const Text('OK'),
-            onPressed: () => Navigator.pop<String>(context, 'OK')
-          ),
-        ],
-      ),
-    ).then<void>((value) => resultAlert(value));
-  }
-
-  void resultAlert(String value){
-    setState(() {
-      _message = 'selected $value';
-    });
-  }
-
-  void showPicker(){
-    showModalBottomSheet<void>(
-      context: context,
-      builder: (BuildContext context){
-        return CupertinoPicker(
-          backgroundColor: CupertinoColors.black,
-          itemExtent: 50.0,
-          children: <Widget>[
-            Text(
-              'One',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              'Two',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              'Three',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              'Four',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-              ),
-            ),
-            Text(
-              'Five',
-              style: TextStyle(
-                fontSize: 32.0,
-                fontWeight: FontWeight.w400,
-                color: Colors.white,
-              ),
-            ),
-          ],
-          onSelectedItemChanged: (int value){
-            print('pick $value');
-          },
-        );
-      },
-    );
-  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
-          backgroundColor: Color.fromARGB(255, 235, 235, 235),
-          appBar: AppBar(
-            title: Text('Qupertino layout App'),
-          ),
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            mainAxisSize: MainAxisSize.max,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: <Widget>[
-              Text(
-                _message,
-                style: TextStyle(
-                  fontSize: 32.0,
-                  color: const Color(0xFF000000),
-                  fontWeight: FontWeight.w400,
-                  fontFamily: "Roboto",
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-              ),
-              CupertinoSwitch(
-                value: _switch,
-                onChanged: (bool value){
-                  setState(() {
-                    _switch = value;
-                    _message = 'switch $_switch';
-                  });
-                },
-              ),
-              CupertinoSlider(
-                value: _slider,
-                min: 0.0,
-                max: 1.0,
-                divisions: 100,
-                onChanged: (double value){
-                  setState(() {
-                    _slider = value;
-                    _message = 'slider $_slider';
-                  });
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: CupertinoButton(
-                  borderRadius: const BorderRadius.all(Radius.circular(20.0)),
-                  pressedOpacity: 0.5,
-                  color: CupertinoColors.activeBlue,
-                  onPressed: buttonPressed,
-                  padding: EdgeInsets.all(20.0),
-                  child: Text(
-                    "tap this",
-                    style: TextStyle(
-                      fontSize: 28.0,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w400,
-                      fontFamily: "Roboto"
-                    ),
-                  ),
-                ),
-              ),
-            ],
-          ),
-          bottomNavigationBar: CupertinoNavigationBar(
-            leading: Icon(CupertinoIcons.left_chevron),
-            middle: Text('Navigation'),
-            trailing: IconButton(
-              icon: Icon(CupertinoIcons.right_chevron),
-              onPressed: showPicker,
-            ),
-          ),
+      backgroundColor: Color.fromARGB(255, 255, 255, 255),
+      appBar: AppBar(
+        title: Text(
+          'App Name',
+        ),
+      ),
+      body: Center(
+        child: MyRenderBoxWidget(),
+      ),
     );
+  }
+}
+
+class MyRenderBoxWidget extends SingleChildRenderObjectWidget{
+  @override
+  RenderObject createRenderObject(BuildContext context){
+    return _MyRenderBox();
+  }
+}
+
+class _MyRenderBox extends RenderBox {
+  @override
+  bool hitTest(HitTestResult result, {@required Offset position}) {
+    return true;
+  }
+  @override
+  void paint(PaintingContext context, Offset offset) {
+    Canvas c = context.canvas;
+    int dx = offset.dx.toInt();
+    int dy = offset.dy.toInt();
+    Paint p = Paint();
+    p.style = PaintingStyle.fill;
+    p.color = Color.fromARGB(150, 0, 200, 255);
+    Rect r = Rect.fromLTWH(dx + 50.0, dy + 50.0, 150.0, 150.0);
+    c.drawRect(r, p);
+    p.style = PaintingStyle.stroke;
+    p.color = Color.fromARGB(150, 200, 0, 255);
+    p.strokeWidth = 10.0;
+    r = Rect.fromLTWH(dx + 100.0, dy + 100.0, 150.0, 150.0);
+    c.drawRect(r, p);
   }
 }
