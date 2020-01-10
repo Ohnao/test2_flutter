76019b586ec1f36a0a1278f878abc7837c0da288
Ohnao <naoya.ohsaki@gaiax.com>
2020-01-10 17:07:07 +0900

    text routing

--- lib/main.dart
+++ lib/main.dart
@@ -13,17 +13,45 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
+  FirstScreen({Key key}) : super(key: key);
+
+  @override
+  _FirstScreenState createState() => new _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  final _controller = TextEditingController();
+  String _input;
+
   @override
   Widget build (BuildContext context){
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Container(
-          child: Text('Home Screen'),
-        )
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Text(
+            'Home Screen',
+            style: TextStyle(
+              fontSize: 32.0,
+              color: const Color(0xFF000000),
+              fontWeight: FontWeight.w300,
+              fontFamily: "Roboto",
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+          ),
+          TextField(
+            controller: _controller,
+            onChanged: changeField,
+          ),
+        ],
       ),
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: 1,
@@ -41,15 +69,20 @@ class FirstScreen extends StatelessWidget {
           if(value == 1)
             Navigator.push(
               context,
-              MaterialPageRoute(builder:(context) => SecondScreen()),
+              MaterialPageRoute(builder:(context) => SecondScreen(_input)),
             );
         },
       ),
     );
   }
+  void changeField(String val) => _input = val;
 }
 
 class SecondScreen extends StatelessWidget {
+  final String _value;
+
+  SecondScreen(this._value);
+
   @override
   Widget build (BuildContext context){
     return Scaffold(
@@ -58,7 +91,7 @@ class SecondScreen extends StatelessWidget {
       ),
       body: Center(
         child: Container(
-          child: Text('Next Screen'),
+          child: Text('You typed $_value'),
         )
       ),
       bottomNavigationBar: BottomNavigationBar(
