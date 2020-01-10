14d0a025279a5173d5656677b173d85fd406a49c
Ohnao <naoya.ohsaki@gaiax.com>
2020-01-10 12:04:49 +0900

    about hittestentry

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/rendering.dart';
-import 'package:flutter/services.dart';
-
-import 'dart:typed_data';
-import 'dart:ui' as ui;
 
 void main() => runApp(new MyApp());
 
@@ -51,55 +47,32 @@ class MyRenderBoxWidget extends SingleChildRenderObjectWidget{
 }
 
 class _MyRenderBox extends RenderBox {
-  ui.Image _img;
+  Offset _pos;
 
   @override
   bool hitTest(HitTestResult result, {@required Offset position}) {
+    result.add(BoxHitTestEntry(this, position));
     return true;
   }
 
-  _MyRenderBox(){
-    loadAssetImage('planet.jpg');
+  @override
+  void handleEvent(PointerEvent event, HitTestEntry entry){
+    super.handleEvent(event, entry);
+    _pos = event.position;
+    markNeedsPaint();
   }
-
-  loadAssetImage(String fname) => rootBundle.load(
-    "images/$fname"
-  ).then((bd){
-    Uint8List u8lst = Uint8List.view(bd.buffer);
-    ui.instantiateImageCodec(u8lst).then((codec) {
-      codec.getNextFrame().then(
-        (frameInfo){
-          _img = frameInfo.image;
-          markNeedsPaint();
-          print("_img created $_img");
-        }
-      );
-    });
-  });
 
   @override
   void paint(PaintingContext context, Offset offset){
     Canvas c = context.canvas;
-    double dx = offset.dx + 30.0;
-    double dy = offset.dy + 30.0;
 
-    if (_img != null){
-      Rect r0 = Rect.fromLTWH(0.0, 0.0, _img.width.toDouble(), _img.height.toDouble());
-      Rect r = Rect.fromLTWH(dx + 0.0, dy + 0.0, 300, 300);
-      c.drawImageRect(_img, r0, r, Paint());
-      print('draw _img');
-    } else {
-      print('-img is null');
-    }
-
-    Paint p = Paint();
-    p.style = PaintingStyle.fill;
-    p.blendMode = BlendMode.difference;
-    for (var i = 0; i < 10; i++){
-      for (var j = 0; j < 10; j++){
-        p.color = Color.fromARGB(255, 25 * i, 0, 25 * j);
-        Rect r = Rect.fromLTWH(dx + 30.0 * i, dy + 30.0 * j, 30.0, 30.0);
-        c.drawOval(r, p);
+    c.drawColor(Colors.black, BlendMode.clear);
+    if (_pos != null){
+      Paint p = Paint();
+      p.style = PaintingStyle.fill;
+      for (var i = 0; i < 10; i++){
+        p.color = Color.fromARGB(50, 255, 255, 255);
+        c.drawCircle(_pos, i * 5.0, p);
       }
     }
   }
